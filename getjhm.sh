1f3420a3636bb03869a43db3ebacf237cf93d390
梅丹隆 <meidanlong@qq.com>
2021-01-04 11:48:42 +0800

    add jhm

---
+++ getjhm.sh
@@ -0,0 +1,14 @@
+
+# cd ~/Document/maoyan/sth/draft/jhm/
+
+curl -O http://idea.medeming.com/jets/images/jihuoma.zip
+
+unar -e GBK jihuoma.zip
+
+# echo '⬇️'
+
+cat ./jihuoma/2018.1之后的版本用这个.txt | pbcopy
+
+rm -rf jihuoma
+
+rm jihuoma.zip
