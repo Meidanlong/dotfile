2fed89ac1b82b0d8e200287b24c90c94da59f18d
梅丹隆 <meidanlong@qq.com>
2021-04-07 14:24:48 +0800

    update

--- getjhm.sh
+++ getjhm.sh
@@ -1,13 +1,11 @@
 
-# cd ~/Document/maoyan/sth/draft/jhm/
-
 curl -O http://idea.medeming.com/jets/images/jihuoma.zip
 
 unar -e GBK jihuoma.zip
 
 # echo '⬇️'
 
-cat ./jihuoma/2018.1之后的版本用这个.txt | pbcopy
+cat ./jihuoma/2018.2之后的版本用这个.txt | pbcopy
 
 rm -rf jihuoma
 
